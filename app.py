cabba0f4b2988408afbe6b915a96a8cfb5b7a642
Eduardo <eduardo_1999sosa@hotmail.com>
2025-05-29 11:13:53 -0500

    Se modifico descripcion y front

--- app.py
+++ app.py
@@ -9,24 +9,26 @@ def home():
         'nombre': 'Eduardo Velazquez Sosa',
         'puesto': 'Aspirante a Data Scientist',
         'foto':'https://png.pngtree.com/thumb_back/fw800/background/20221108/pngtree-traveler-man-enjoying-by-scenic-of-mountains-person-young-scenics-photo-image_40834320.jpg',
-        'resumen': 'Ingeniero en Sistemas Computacionales con habilidades en Python, HTML/CSS, y fundamentos de Ciencia de Datos, actualmente en formación en Científico de Datos, con cinco años de experiencia en la secretaria de marina, en donde desarrollado una fuerte ética de trabajo, disciplina y habilidades para resolver problemas bajo presión. Busco mi primera oportunidad en el campo de la Ciencia de Datos, donde pueda aplicar y expandir mis conocimientos técnicos en un entorno colaborativo.',
+        'resumen': 'Egresado de la Ingeniería en Sistemas Computacionales experiencia en análisis estadístico y funciones administrativas en entornos de alta exigencia como el Aeropuerto Internacional de Cancún. Me desempeñé como encargado del área de estadísticas delictivas, desarrollando reportes mensuales y seguimiento de indicadores clave. Con habilidades en Excel, Python y SQL, capacidad para trabajar bajo presión, actitud proactiva y excelente redacción, busco integrarme como Asistente Comercial en una empresa dinámica donde pueda aportar y seguir desarrollándome profesionalmente',
         'experiencia': [
-            {'puesto': 'Militar', 'empresa': 'Marina de México', 'años': '2017-2022'}
+            {'puesto': 'Encargado del Area de Estadisticas / Administrativo', 'empresa': 'Aeropuerto Internacional de Cancun', 'años': '2021-2025'}
             
         ],
         'educacion': [
-            {'titulo': 'Ingeniería en Sistemas Computacionales', 'institucion': 'Universidad Aztlan', 'años': '2021-2024', 'resumen': 'En la carrera adquiere la pasion por las estadisticas y el amor por la ciencia de datos, por lo que adquiere una base solida.'},
-            {'titulo': 'Diplomado en Desarrollo Web', 'institucion': 'Universidad Aztlan', 'años': '2024', 'resumen':'Realice varios proyecto en el que destaco la creacion de una Tienda Online de Ropa utilizando tecnología comoHTML/CSS, JavaScript, PHP, SQL.'},
-            {'titulo': 'Maestria en Analisis e Interpretacion de Datos Digitales', 'institucion': 'Universidad Oriente', 'años': 'Estudiante', 'resumen':'en proceso'} 
+            {'titulo': 'Ingeniería en Sistemas Computacionales', 'institucion': 'Universidad Aztlan', 'años': '2021-2024', 'resumen': 'Formación orientada al diseño, desarrollo e implementación de soluciones tecnológicas. Aprendí programación, bases de datos, redes, inteligencia artificial, análisis de datos, ciberseguridad y arquitectura de software. Participé en proyectos que integran tanto hardware como software para resolver problemas reales con tecnología.'},
+            {'titulo': 'Diplomado en Desarrollo Web', 'institucion': 'Universidad Aztlan', 'años': '2024', 'resumen':'Formación práctica en el desarrollo de sitios web dinámicos y responsivos. Aprendí a estructurar y diseñar páginas con HTML y CSS, implementar interactividad mediante JavaScript, y desarrollar funcionalidades del lado del servidor utilizando PHP. Incluyó buenas prácticas de programación, control de versiones con Git, y trabajo con formularios, bases de datos y despliegue web.'},
+            {'titulo': 'Maestria en Analisis e Interpretacion de Datos Digitales (En curso)', 'institucion': 'Universidad Oriente', 'años': '2025-2026', 'resumen':'Formación orientada al dominio de herramientas y metodologías para la recopilación, procesamiento, análisis e interpretación de grandes volúmenes de datos. El plan de estudios abarca temas clave como ciencia de datos, inteligencia artificial, modelos estadísticos, calidad y preparación de datos, ciberseguridad, bases de datos y programación avanzada, integrando enfoques técnicos y estratégicos para la toma de decisiones basadas en datos.'} 
         ],
         'cursos':[
-            {'curso':'Python','icono':'/static/icons/python.png', 'acerca':'Complete el curso de Python en Coderhouse, con temas como Django Framework y su MVT basado en Modelos, Vistas y Templates, asi como Git y Github.','año':'2022','instituto':'Coderhouse','certificado':'/static/file/Python.png'},
-            {'curso': 'JavaScript','icono':'/static/icons/js.png', 'acerca':'Complete el curso de JavaScript en Cordehouse, en donde adquiri conocimiento sobre HTML/CSS y Javascript.', 'año': '2023','instituto':'Coderhouse','certificado':'/static/file/JS.png'} 
-            ],
+            {'curso':'Python','icono':'/static/icons/python.png', 'acerca':'Aprendí desde los fundamentos del lenguaje hasta el desarrollo de aplicaciones web utilizando Flask y Django. Abarqué programación estructurada, manejo de archivos, APIs, entornos virtuales y creación de backends con bases de datos.','año':'2022','instituto':'Coderhouse','certificado':'/static/file/Python.png'},
+            {'curso': 'JavaScript','icono':'/static/icons/js.png', 'acerca':'Dominié los principios del lenguaje JavaScript para desarrollo web, incluyendo manipulación del DOM, eventos, estructuras de datos, funciones asincrónicas y consumo de APIs. Realicé proyectos interactivos desde cero.', 'año': '2023','instituto':'Coderhouse','certificado':'/static/file/JS.png'},
+            {'curso': 'SQL','icono':'/static/icons/db.png', 'acerca':'Aprendí a diseñar, consultar y gestionar bases de datos relacionales. Utilicé comandos DDL y DML, funciones agregadas, filtros y relaciones entre tablas. Manejo de bases de datos con MySQL.', 'año': '2025','instituto':'Coderhouse','certificado':'imagen'},
+            {'curso': 'Data Science','icono':'/static/icons/ds.png', 'acerca':'Profundicé en análisis de datos con Python, utilizando bibliotecas como pandas, matplotlib, seaborn y scikit-learn. Trabajé con modelos de machine learning, validación cruzada y análisis predictivo con datos reales.', 'año': '2025','instituto':'Coderhouse','certificado':'imagen'},
+        ],
         'habilidades': [
-            {'interpersonales':['Resolución de Problemas','Trabajo en Equipo', 'Autodidacta','Curiosidad']},
-            {'tecnicas': ['Python', 'SQL', 'Pandas', 'Automatización', 'Análisis de Datos', 'Flask']},
-            {'idiomas':{'Español - Nativo':'Nativo','Ingles - A2':'A2'}},
+            {'interpersonales':['Resolución de Problemas','Trabajo en Equipo', 'Autodidacta','Aprendizaje continuo']},
+            {'tecnicas': ['Python', 'SQL','Excel', 'Pandas', 'Automatización', 'Análisis de Datos', 'Flask']},
+            {'idiomas':{'Español - Nativo':'','Ingles - En proceso':''}},
             ],
         'contacto': {
             'correo': 'eduardo99sosa@gmail.com.com',
